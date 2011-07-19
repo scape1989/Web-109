b9ceb87ddf357a2bf2b89d6dce47fd901379223d
scape1989 <670985623@qq.com>
2011-07-19 10:29:36 +0800

    initialize Web-109 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web109::Application.config.secret_token = '89fd600496460759a76c74d5324378320ea3e91c4ca3a9745e03bf40534c88cab7b2eabca570a2298a4635f5ccf0ad417cc51522ea9eb67df3a60443f2e033a3'
